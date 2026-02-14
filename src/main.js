2749015409fdfc67115bae48cd3a369d20c92ee9
Pichele19811003 <pichelegeorgesa@gmail.com>
2026-02-14 09:18:23 +0000

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+          width: 1400,
+          height: 900,
+          webPreferences: {
+                  nodeIntegration: false,
+                  contextIsolation: true
+          }
+    });
+
+  win.loadFile('src/renderer/index.html');
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => app.quit());
